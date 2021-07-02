61aef4977874937764099bb34665589ee00f7c0d
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2021-07-02 13:20:14 -0400

    Load text data from files and process/clean dataset

--- models/model_{5}/model_5.py
+++ models/model_{5}/model_5.py
@@ -15,3 +15,29 @@ import matplotlib.pyplot as plt
 import numpy as np
 import glob
 import string
+
+# In[44]:
+
+
+charlist = string.ascii_lowercase + ' \n' + string.digits
+print(charlist)
+
+
+# In[78]:
+
+
+dataset = []
+labels = []
+
+for category, value in [('neg', 0), ('pos', 1)]:
+    filenames = glob.glob(f'../train/{category}/*.txt')[:100]
+    for i, f in enumerate(filenames):
+        with open(f) as textfile:
+            p = string.punctuation
+            content = textfile.read().translate(str.maketrans(p, ' '*len(p)))
+            content = content.lower()
+            content = ' '.join(content.split())
+            dataset.append(content)
+            labels.append(value)
+            if i < 5:
+                print(i, content[:200]+'...', '\n')
